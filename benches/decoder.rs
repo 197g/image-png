914c7f0d0e31c69c1601124f199d51f4a7f27f4d
Andreas Molzer <andreas.molzer@gmx.de>
2021-10-10 16:28:01 +0200

    Add benchmarks for normalization decoding
    This set of parameters is, in particular, utilized heavily in `image`
    and probably in other dependencies that use the defaults from 0.16 and
    earlier. So it makes sense to also optimize this configuration of
    parameters.

--- benches/decoder.rs
+++ benches/decoder.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
 use criterion::{criterion_group, criterion_main, Criterion, Throughput};
-use png::Decoder;
+use png::{Decoder, Transformations};
 
 fn load_all(c: &mut Criterion) {
     for file in fs::read_dir("tests/benches/").unwrap() {
@@ -12,7 +12,8 @@ fn load_all(c: &mut Criterion) {
             }
 
             let data = fs::read(entry.path()).unwrap();
-            bench_file(c, data, entry.file_name().into_string().unwrap());
+            bench_file_and_normalize(c, &data, entry.file_name().into_string().unwrap());
+            bench_file(c, &data, entry.file_name().into_string().unwrap());
         }
     }
 }
@@ -20,7 +21,7 @@ fn load_all(c: &mut Criterion) {
 criterion_group!(benches, load_all);
 criterion_main!(benches);
 
-fn bench_file(c: &mut Criterion, data: Vec<u8>, name: String) {
+fn bench_file(c: &mut Criterion, data: &[u8], name: String) {
     let mut group = c.benchmark_group("decode");
     group.sample_size(20);
 
@@ -30,9 +31,30 @@ fn bench_file(c: &mut Criterion, data: Vec<u8>, name: String) {
     let info = reader.next_frame(&mut image).unwrap();
 
     group.throughput(Throughput::Bytes(info.buffer_size() as u64));
-    group.bench_with_input(name, &data, |b, data| {
+    group.bench_with_input(name, data, |b, data| {
         b.iter(|| {
-            let decoder = Decoder::new(data.as_slice());
+            let decoder = Decoder::new(data);
+            let mut decoder = decoder.read_info().unwrap();
+            decoder.next_frame(&mut image).unwrap();
+        })
+    });
+}
+
+fn bench_file_and_normalize(c: &mut Criterion, data: &[u8], name: String) {
+    let mut group = c.benchmark_group("normalized");
+    group.sample_size(20);
+
+    let mut decoder = Decoder::new(&*data);
+    decoder.set_transformations(Transformations::normalize_to_color8());
+    let mut reader = decoder.read_info().unwrap();
+    let mut image = vec![0; reader.output_buffer_size()];
+    let info = reader.next_frame(&mut image).unwrap();
+
+    group.throughput(Throughput::Bytes(info.buffer_size() as u64));
+    group.bench_with_input(name, data, |b, data| {
+        b.iter(|| {
+            let mut decoder = Decoder::new(data);
+            decoder.set_transformations(Transformations::normalize_to_color8());
             let mut decoder = decoder.read_info().unwrap();
             decoder.next_frame(&mut image).unwrap();
         })
